4b0de6a5c39776e68eafeae24b8a4b2ce9e921f7
Sandesh Sardar <sandesh.sardar@klm.com>
2022-07-25 12:48:00 +0530

    Added network layer

---
+++ TestCase/ViewModel/HomeViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewModel.swift
+//  TestCase
+//
+//  Created by 1923800 on 25/07/22.
+//
+
+import Foundation
+import CoreData
+import SwiftUI
+
+class HomeViewModel: ObservableObject {
+
+    @Published var results = [ResultItem]()
+    @Published var coreDM: PersistenceController = PersistenceController()
+
+    func getData() {
+        guard let gUrl = URL(
+            string: "https://api.artic.edu/api/v1/artworks"
+        ) else { return }
+
+        Task {
+            do {
+                let (data, _) = try await URLSession.shared.data(from: gUrl)
+                let response = try JSONDecoder()
+                    .decode(ResponseData.self, from: data)
+                DispatchQueue.main.async { [weak self] in
+                    self?.results = response.data ?? []
+                }
+            } catch {
+                print("*** ERROR ***")
+            }
+        }
+    }
+}
